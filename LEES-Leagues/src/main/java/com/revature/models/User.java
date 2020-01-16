edaf15f03d9035b4a50035fc6cab311a16a30b47
lmr608 <ross.lawrencem@gmail.com>
2020-01-15 21:33:23 -0500

    Created models

--- LEES-Leagues/src/main/java/com/revature/models/User.java
+++ LEES-Leagues/src/main/java/com/revature/models/User.java
@@ -1,5 +1,153 @@
 package com.revature.models;
 
+import java.util.List;
+import java.util.Objects;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import com.revature.model.Crimes;
+
+@Entity
+@Table(name="User")
 public class User {
 
+	@Id
+	@Column(name="user_id")
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int userId;
+	
+	@Column(name="first_name", nullable=false)
+	private String firstName;
+	
+	@Column(name="last_name", nullable=false)
+	private String lastName;
+	
+	@Column(name="username", nullable=false, unique=true)
+	private String username;
+	
+	@Column(name="password", nullable=false)
+	private String password;
+	
+	@Column(name="email", nullable=false, unique=true)
+	private String email;
+	
+	@ManyToMany(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
+	private List<Team> team;
+
+	public User() {
+		super();
+	}
+	
+	public User(String firstName, String lastName, String username, String password, String email, List<Team> team) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.team = team;
+	}
+
+	public User(int userId, String firstName, String lastName, String username, String password, String email,
+			List<Team> team) {
+		super();
+		this.userId = userId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+		this.team = team;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Team> getTeam() {
+		return team;
+	}
+
+	public void setTeam(List<Team> team) {
+		this.team = team;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, firstName, lastName, password, team, userId, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof User)) {
+			return false;
+		}
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(firstName, other.firstName)
+				&& Objects.equals(lastName, other.lastName) && Objects.equals(password, other.password)
+				&& Objects.equals(team, other.team) && userId == other.userId
+				&& Objects.equals(username, other.username);
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", firstName=" + firstName + ", lastName=" + lastName + ", username="
+				+ username + ", password=" + password + ", email=" + email + ", team=" + team + "]";
+	}
+	
 }
