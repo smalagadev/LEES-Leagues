c6db8326c8271536304bcb6f85972107d9d63919
Steffano <smalagadev@gmail.com>
2020-01-16 22:40:54 -0500

    Import filepaths changed

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { CustomMaterialModule } from './core/material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import {FormsModule} from '@angular/forms'
 
@@ -10,13 +10,21 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { UserComponent } from './user/user.component';
 import { RegisterComponent } from './register/register.component';
+import { ComponentsComponent } from './src/app/components/components.component';
+import { NavbarComponent } from './src/app/components/navbar/navbar.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
+import { RegistrationFormComponent } from './components/registration-form/registration-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     UserComponent,
-    RegisterComponent
+    RegisterComponent,
+    ComponentsComponent,
+    NavbarComponent,
+    LoginFormComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule,
