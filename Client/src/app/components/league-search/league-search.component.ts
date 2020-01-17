ac83e499de167f9398f0efb28f8549f70703042c
Steffano <smalagadev@gmail.com>
2020-01-16 23:04:19 -0500

    Components initiated

---
+++ Client/src/app/components/league-search/league-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-league-search',
+  templateUrl: './league-search.component.html',
+  styleUrls: ['./league-search.component.css']
+})
+export class LeagueSearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
