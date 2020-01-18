e5d759863f0ec1899416dd48bc4ca339e3217583
jeffersone9 <jeffersone9@gmail.com>
2020-01-17 20:09:14 -0500

    fixed applicationContext and used hibernate 5 criteria query

--- LEES-Leagues/src/main/java/com/revature/dao/UserDao.java
+++ LEES-Leagues/src/main/java/com/revature/dao/UserDao.java
@@ -1,5 +1,7 @@
 package com.revature.dao;
 
+import java.util.List;
+
 import com.revature.models.User;
 
 public interface UserDao {
@@ -7,5 +9,6 @@ public interface UserDao {
 	public User login(String username, String password);
 	public User getByusername(String username);
 	public void insert(User user);
-	
+	public User findById(int id);
+	public List<User> findAll();
 }
--- LEES-Leagues/src/main/java/com/revature/dao/UserDaoImpl.java
+++ LEES-Leagues/src/main/java/com/revature/dao/UserDaoImpl.java
@@ -1,93 +1,67 @@
 package com.revature.dao;
 
-import java.sql.Array;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
 import org.apache.log4j.Logger;
 import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.revature.models.User;
-import com.revature.util.ConnectionUtil;
-import com.revature.util.HibernateUtil;
 
+@Repository
 public class UserDaoImpl implements UserDao {
 
-	private static Logger logger = Logger.getLogger(UserDaoImpl.class);
+	@Autowired
+	private SessionFactory sf;
 	
+	private static Logger logger = Logger.getLogger(UserDaoImpl.class);
+
 	@Override
 	public User login(String username, String password) {
-		
-		try (Connection conn = ConnectionUtil.getConnection()) {
-			
-			String sql = "SELECT * FROM project1.users WHERE username = ? AND password = ?;";
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			stmt.setString(1, username);
-			stmt.setString(2, password);
-			ResultSet rs = stmt.executeQuery();
-			
-			while (rs.next()) {
-				int uid = rs.getInt("user_id");
-				String ufirst_name = rs.getString("first_name");
-				String ulast_name = rs.getString("last_name");
-				String uusername = rs.getString("username");
-				String upassword = rs.getString("password");
-				String uemail = rs.getString("email");
-				Array uteam_id = rs.getArray("team_id");
-				
-				User u = new User(uid, ufirst_name, ulast_name, uusername, upassword, uemail, uteam_id);
-				return u;
-			}
-			rs.close();
-		} catch (SQLException e) {
-			logger.warn("Unable to get User from database", e);
-			e.printStackTrace();
-		}
+		// TODO Auto-generated method stub
 		return null;
 	}
-	
+
 	@Override
 	public User getByusername(String username) {
-
-		try (Connection conn = ConnectionUtil.getConnection()) {
-			
-			String sql = "SELECT * FROM project1.users WHERE username = ? AND password = ?;";
-			PreparedStatement stmt = conn.prepareStatement(sql);
-			stmt.setString(1, username);
-			ResultSet rs = stmt.executeQuery();
-			
-			while (rs.next()) {
-				int uid = rs.getInt("user_id");
-				String ufirst_name = rs.getString("first_name");
-				String ulast_name = rs.getString("last_name");
-				String uusername = rs.getString("username");
-				String upassword = rs.getString("password");
-				String uemail = rs.getString("email");
-				Array uteam_id = rs.getArray("team_id");
-				
-				User u = new User(uid, ufirst_name, ulast_name, uusername, upassword, uemail, uteam_id);
-				return u;
-			}
-			rs.close();
-		} catch (SQLException e) {
-			logger.warn("Unable to get User from database", e);
-			e.printStackTrace();
-		}
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public void insert(User user) {
-
-		Session ses = HibernateUtil.getSession();
-		Transaction tx = ses.beginTransaction();
-		
-		ses.save(user);
-		tx.commit();
-		ses.close();
+		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public User findById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	@Override
+	@Transactional(propagation=Propagation.REQUIRED)
+	public List<User> findAll() {
+		Session s = sf.getCurrentSession();
+		CriteriaBuilder builder = s.getCriteriaBuilder();
+		CriteriaQuery<User> query = builder.createQuery(User.class);
+		Root<User> root = query.from(User.class);
+		query.select(root);
+		Query<User> q = s.createQuery(query);
+		return q.getResultList();
+	}
+	
+
 
 }
