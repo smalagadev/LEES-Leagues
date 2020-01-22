edf77aa4aa095712cb78db99b01179cbf61b19bd
Steffano <smalagadev@gmail.com>
2020-01-22 10:57:26 -0500

    Spacing changed

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,8 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {FormsModule, FormControl, FormGroup} from '@angular/forms';
-
 import {MatTabsModule} from '@angular/material';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
