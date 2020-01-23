134502a0b71bd6b0a86a00b0971797b30a4ad010
lmr608 <ross.lawrencem@gmail.com>
2020-01-23 10:18:10 -0500

    Added league and sport daos, services, and tests

--- LEES-Leagues/src/main/java/com/revature/models/League.java
+++ LEES-Leagues/src/main/java/com/revature/models/League.java
@@ -1,6 +1,8 @@
 package com.revature.models;
 
 import java.io.Serializable;
+import java.util.Objects;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.FetchType;
@@ -24,5 +26,76 @@ public class League implements Serializable {
 	
 	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY)
 	@JoinColumn(name="sport_fk")
-	private Sport sportHolder;
+	private int sportHolder;
+
+	public League() {
+		super();
+	}
+
+	public League(int leagueId, String leagueName, int sportHolder) {
+		super();
+		this.leagueId = leagueId;
+		this.leagueName = leagueName;
+		this.sportHolder = sportHolder;
+	}
+
+	public League(int leagueId, String leagueName) {
+		super();
+		this.leagueId = leagueId;
+		this.leagueName = leagueName;
+	}
+
+	public League(String leagueName, int sportHolder) {
+		super();
+		this.leagueName = leagueName;
+		this.sportHolder = sportHolder;
+	}
+
+	public int getLeagueId() {
+		return leagueId;
+	}
+
+	public void setLeagueId(int leagueId) {
+		this.leagueId = leagueId;
+	}
+
+	public String getLeagueName() {
+		return leagueName;
+	}
+
+	public void setLeagueName(String leagueName) {
+		this.leagueName = leagueName;
+	}
+
+	public int getSportHolder() {
+		return sportHolder;
+	}
+
+	public void setSportHolder(int sportHolder) {
+		this.sportHolder = sportHolder;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(leagueId, leagueName, sportHolder);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof League)) {
+			return false;
+		}
+		League other = (League) obj;
+		return leagueId == other.leagueId && Objects.equals(leagueName, other.leagueName)
+				&& Objects.equals(sportHolder, other.sportHolder);
+	}
+
+	@Override
+	public String toString() {
+		return "League [leagueId=" + leagueId + ", leagueName=" + leagueName + ", sportHolder=" + sportHolder + "]";
+	}
+	
 }
--- LEES-Leagues/src/main/java/com/revature/models/Sport.java
+++ LEES-Leagues/src/main/java/com/revature/models/Sport.java
@@ -1,10 +1,9 @@
 package com.revature.models;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 public class Sport implements Serializable {
@@ -13,9 +12,57 @@ public class Sport implements Serializable {
 
 	@Id
 	@Column(name="sport_id", nullable=false, unique=true)
-	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int sportId;
 
 	@Column(name="sport_name", nullable=false)
 	private String sportName;
+
+	public Sport() {
+		super();
+	}
+
+	public Sport(int sportId, String sportName) {
+		super();
+		this.sportId = sportId;
+		this.sportName = sportName;
+	}
+
+	public int getSportId() {
+		return sportId;
+	}
+
+	public void setSportId(int sportId) {
+		this.sportId = sportId;
+	}
+
+	public String getSportName() {
+		return sportName;
+	}
+
+	public void setSportName(String sportName) {
+		this.sportName = sportName;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(sportId, sportName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof Sport)) {
+			return false;
+		}
+		Sport other = (Sport) obj;
+		return sportId == other.sportId && Objects.equals(sportName, other.sportName);
+	}
+
+	@Override
+	public String toString() {
+		return "Sport [sportId=" + sportId + ", sportName=" + sportName + "]";
+	}
+	
 }
