de10a70b9f5932ba566b87a4921216553a05a93b
Steffano <smalagadev@gmail.com>
2020-01-22 20:03:03 -0500

    news api http requests populates homepage with first 20 articles

--- Client/src/app/services/articles.service.ts
+++ Client/src/app/services/articles.service.ts
@@ -10,7 +10,8 @@ export class ArticlesService {
 
   constructor(private http: HttpClient) { }
 
-  viewAll(){
+
+  viewAll(){// currently retrieves general sports news
     return this.http.get("https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=b68de02f312240cd9a3aed77b8476c18");
   }
 }
