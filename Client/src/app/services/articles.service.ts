ff7f3de6a9790ad6abc3ce0db670daacc36dcd68
Steffano <smalagadev@gmail.com>
2020-01-23 13:19:03 -0500

    news feed component HTML rewritten

--- Client/src/app/services/articles.service.ts
+++ Client/src/app/services/articles.service.ts
@@ -10,8 +10,9 @@ export class ArticlesService {
 
   constructor(private http: HttpClient) { }
 
+  api_key="b68de02f312240cd9a3aed77b8476c18";
 
   viewAll(){// currently retrieves general sports news
-    return this.http.get("https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=b68de02f312240cd9a3aed77b8476c18");
+    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=${this.api_key}`);
   }
 }
