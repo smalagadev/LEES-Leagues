9330f0d2191328f2e431213fdcf7e521fc32b1c4
Steffano <smalagadev@gmail.com>
2020-01-23 19:26:23 -0500

    GameSchedulesComponent imported corrected

--- Client/src/app/components/home/home.component.ts
+++ Client/src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { GameScheduleComponent } from './../game-schedules/game-schedules.component';
+import { GameSchedulesComponent } from './../game-schedules/game-schedules.component';
 import { NewsFeedComponent } from './../news-feed/news-feed.component';
 
 @Component({
