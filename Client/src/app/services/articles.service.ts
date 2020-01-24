f4069f48b81e86118c289cdeba541c5fea5dcfc2
Steffano <smalagadev@gmail.com>
2020-01-24 13:36:13 -0500

    getAll() changed to getTopHeadlines(), getByTopic() added

--- Client/src/app/services/articles.service.ts
+++ Client/src/app/services/articles.service.ts
@@ -12,7 +12,10 @@ export class ArticlesService {
 
   api_key="b68de02f312240cd9a3aed77b8476c18";
 
-  viewAll(){// currently retrieves general sports news
+  getTopHeadlines(){// currently retrieves general sports news
     return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=${this.api_key}`);
   }
-}
+
+  getByTopic(topic: string){
+    return this.http.get(`https://newsapi.org/v2/everything?q=${topic}&category=sports&apiKey=${this.api_key}`);
+  }
