da860a7110d991202da78956a3778ce35e32f5d0
lmr608 <ross.lawrencem@gmail.com>
2020-01-25 18:24:29 -0500

    Started team page

--- Client/src/app/components/team/team.component.ts
+++ Client/src/app/components/team/team.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ArticlesService } from 'src/app/services/articles.service';
+import { Article } from './../../models/article';
 
 @Component({
   selector: 'app-team',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./team.component.css']
 })
 export class TeamComponent implements OnInit {
+  articles: Article[] = [];
 
-  constructor() { }
+  constructor(private as: ArticlesService) { }
 
   ngOnInit() {
+    this.as.getByTopic.subscribe(
+      (response: any) => {
+      this.articles = response.articles;
+    });
   }
 
+  panelOpenState = true;
 }
